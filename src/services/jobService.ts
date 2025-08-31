f159cbb4c2c6e66a3fb34f6084e5088ad0337288
Jayantbhoj <bhojtanay@gmail.com>
2025-08-31 22:46:54 +0530

    dockerised

--- src/services/jobService.ts
+++ src/services/jobService.ts
@@ -4,7 +4,6 @@ import type { Job } from "../types";
 
 export const fetchJobs = async (): Promise<Job[]> => {
   const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
-  // map mock API to Job type
   return res.data.slice(0, 20).map((job: any) => ({
     id: job.id,
     title: job.title,
